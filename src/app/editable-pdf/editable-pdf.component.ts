19d269dfbb0173d16c02f0692ae42f486c7da1ac
fardeen ahmad <fardeen.cs@gmail.com>
2020-02-26 23:18:15 +0530

    show-less panel added

--- src/app/editable-pdf/editable-pdf.component.ts
+++ src/app/editable-pdf/editable-pdf.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditablePdfComponent implements OnInit {
 
-  constructor() { }
+  list: Array<any>;
+  isShowLess = true;
+  constructor() {
+    this.list = new Array<any>();
+  }
 
   ngOnInit() {
+    this.getList(20);
+  }
+
+  private getList(length: number) {
+    for (let index = 0; index < length; index++) {
+      this.list.push({
+        label: `Label ${index}`,
+        value: `value ${index + 1}`,
+      });
+    }
+  }
+
+  toggelShowLessPanel() {
+    this.isShowLess = !this.isShowLess;
   }
 
 }
